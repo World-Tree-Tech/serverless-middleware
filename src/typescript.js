2b89037c37693348d10867949befa54f1607814b
Patrick Bell <ext0@users.noreply.github.com>
2021-03-09 17:46:42 -0600

    Update typescript.js
    Remove forced typescript bindings in lib

--- src/typescript.js
+++ src/typescript.js
@@ -43,25 +43,17 @@ function createTSMiddlewareHandler(handlers, pathToRoot) {
   }).join('\n');
 
   return `'use strict';
-
-import { Context } from 'aws-lambda';
-    
-type MiddlewareContext<TResult = any> = Context & { end: () => void, prev: TResult };
-type Handler<TEvent = any, TResult = any> = (
-  event: TEvent,
-  context: MiddlewareContext,
-) => Promise<TResult>;
     
 ${imports}
 
-export const handler: Handler = (event, context) => {
+export const handler = (event: any, context: any) => {
   let end = false;
   context.end = () => end = true;
 
-  const wrappedHandler = (handler: Handler) => (prev: any): Promise<any> => {
-    if (end) return prev;
-    context.prev = prev;
-    return handler(event, context);
+  const wrappedHandler = (handler: any) => (prev: any): Promise<any> => {
+      if (end) return prev;
+      context.prev = prev;
+      return handler(event, context);
   };
 
   return Promise.resolve()
